cc7226ff76474153e2b12f2e799c70b2e11080c4
Samantha Hipple <77363883+Hipples@users.noreply.github.com>
2023-10-11 20:58:44 -0700

    version 2

--- vite-react/image-sliders/src/components/image-slider.jsx
+++ vite-react/image-sliders/src/components/image-slider.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
-import { ArrowBigLeft, ArrowBigRight } from 'lucide-react';
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from 'lucide-react';
 
 import './image-slider.css';
 
 const ImageSlider = ({ imageUrls }) => { 
-  const [index, setIndex] = useState(0);
+  const [image, setImage] = useState(0);
 
   const ShowPrevImage = () => {
-    setIndex(index => {
+    setImage(index => {
       if (index === 0) return imageUrls.length - 1;
       return index - 1;
     });  
   }
 
   const ShowNextImage = () => {
-    setIndex(index => {
+    setImage(index => {
       if (index === imageUrls.length - 1) return 0;
       return index + 1;
     });
@@ -22,7 +22,14 @@ const ImageSlider = ({ imageUrls }) => {
 
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-      <img src={imageUrls[index]} className="img-slider-img" />
+      <div style={{ width: '100%', height: '100%', display: 'flex', overflow: 'hidden' }}>
+        {imageUrls.map(url => (
+          <img 
+            key={url} src={url} 
+            className="img-slider-img"
+            style={{ translate: `${-100 * image}%` }} />
+        ))}
+      </div>
       <button 
         onClick={ShowPrevImage} 
         className='img-slider-btn' 
@@ -35,6 +42,21 @@ const ImageSlider = ({ imageUrls }) => {
         style={{ right: 0 }}>
         <ArrowBigRight />
       </button>
+      <div style={{ 
+        position: 'absolute', 
+        bottom: '.5rem', 
+        left: '50%', 
+        translate: '-50%',
+        display: 'flex',
+        gap: '.25rem' }}>
+          {imageUrls.map((_, index) => (
+            <button 
+              onClick={() => setImage(index)}
+              className='img-slider-dot-btn'>
+                {index === image ? <CircleDot /> : <Circle />}
+            </button>
+          ))}
+      </div>
     </div>
   );
 }
